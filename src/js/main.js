28be7d3910524fcb78332a3eb26b81423b0a3c71
nubenunca <marylinrestrepp@gmail.com>
2024-05-01 16:30:06 -0500

    construcción de nuevo objeto, obteniendo los datos  del formulario

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,26 @@
+let mascotas = [];
+
 const form = document.getElementById("createForm");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("mascota creada exitosamente", event);
 
+  //obtener datos de los input´s (controles de formulario) 
   const formData = new FormData(form);
-  const birthDateFormControl = formData.get("birthDate");
-  console.log(birthDateFormControl);
 
-  console.log("nombre mascota", formData.get("petName"));
-  console.log(`birthDate:${birthDate}`);
-  console.log(`petName:${petName}`);
-
+  //construcción del nuevo objeto 
+  let mascota = {
+    nombre: formData.get("nombre"),
+    especie: "",
+    raza: "",
+    edad: formData.get("edad"),
+    peso: "",
+    estado: "",
+    propietario: formData.get("propietario"),
+    documento: "",
+    telefono: "",
+    correo: "",
+  };
+  mascotas.push(mascota);
+  console.log(mascotas);
 });
-
